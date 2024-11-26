6643a9ad72e020e0c3f846e0af8f84d031ef3ba4
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2024-11-26 15:16:02 +0200

    whatsoever

--- android/app/src/main/kotlin/com/example/hedieaty_app_/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hedieaty_app_/MainActivity.kt
@@ -1,5 +1,7 @@
-package com.example.hedieaty_app_
+package com.example.hedieaty.giftingapp
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // No need for any additional code for v2 embedding
+}
